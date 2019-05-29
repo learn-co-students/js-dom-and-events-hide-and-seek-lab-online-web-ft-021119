3cfec10a9202fe759063b53aace4969268afadb4
joanrig <github email address>
2019-05-29 12:35:32 -0400

    Done.

--- index.js
+++ index.js
@@ -13,41 +13,8 @@ function increaseRankBy(n){
   }
 }
 
+
 function deepestChild(){
-  let nodes =  document.querySelector("div#grand-node")
-  for (let i = 0; i < nodes.length; i++){
-    child = nodes.querySelector(div)
-      if (child) {
-        child.child
-      }
-  }
+  let nodes =  document.getElementById("grand-node").querySelectorAll("div")
+  return nodes[nodes.length- 1];
 }
-
-
-//   Tree.prototype.traverse = function (callback) {
-//   var queue=[this];
-//   var n;
-//
-//   while(queue.length>0) {
-//
-//     n = queue.shift();
-//     callback(n.value);
-//
-//     if (!n.children) {
-//       continue;
-//     }
-//
-//     for (var i = 0; i< n.children.length; i++) {
-//        queue.push(n.children[i]);
-//     }
-//   }
-// };
-
-
-
-
-// function deepestChild(){
-//   let nodes =  document.querySelectorAll("div#grand-node")
-//   let deepest = nodes[nodes.length- 1];
-// }
-// =>  AssertionError: expected { Object () } to equal { Object () }
