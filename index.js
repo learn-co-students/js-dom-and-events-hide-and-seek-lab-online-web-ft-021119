2df80e619c94af23ceab6bcf1eb4209bb86e2683
AudreyJones <audrey.eliz.jones@gmail.com>
2019-06-04 10:58:18 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+getFirstSelector = function (selector) {
+    return document.querySelector(selector)
+}
+
+var nestedTarget = function() {
+    return document.querySelector( '#nested .target' )
+}
+
+
+
