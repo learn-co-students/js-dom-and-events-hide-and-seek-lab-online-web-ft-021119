0994cdba1f92b5449f286fe75ca062ce89dbbc5b
AudreyJones <audrey.eliz.jones@gmail.com>
2019-06-04 11:19:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 
 
-var getFirstSelector = function (selector) {
+const getFirstSelector = function (selector) {
     return document.querySelector(selector)
 }
 
-var nestedTarget = function() {
+const nestedTarget = function() {
     return document.querySelector( '#nested .target' )
 }
 
+const deepestChild = function() {
+    return document.querySelector('#grand-node div div div div')
+}
 
-
+const increaseRankBy = function(n) {
+    let list;
+    list = document.querySelector('.ranked-list')
+    for (i of list) { 
+        return i + n;
+}
+}
